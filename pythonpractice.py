99b26bef0e1dfb86bc8d3acbf6a9327561ba2289
THEMANJH <jonghwan2208@inha.edu>
2025-01-31 16:40:00 +0900

    읽어라.

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,5 +1,5 @@
 '''이건 그냥 파이썬 연습용 페이지구요 일괄 실행이 가능합니다.'''
-print(3+5)
+print(345)
 print(3213*5124)
 print(123+3)
 print('최장혁멍청아')
@@ -7,4 +7,4 @@ print('최장혁멍청아')
 name='최장혁'
 feature='멍청하'
 
-print(f"{name}은 {feature}다")    #여기서 f는 format의 약자로 중괄호 안에 변수를 넣어서 출력할 수 있게 해준다.
+print("name은 feature다")    #여기서 f는 format의 약자로 중괄호 안에 변수를 넣어서 출력할 수 있게 해준다
